bd6e3a96beacd464e185f955a38fd1a3b523848c
bartek3xe <bartek3xe@gmail.com>
2023-12-08 23:18:47 +0100

    Refactor security controller

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -11,13 +13,14 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'dev_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('dev_admin');
         }
 
         $form = $this->createForm(LoginType::class);
+        $form->add('submit', SubmitType::class);
 
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
@@ -25,13 +28,13 @@ class SecurityController extends AbstractController
         return $this->render('admin/security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
-            'form' => $form
+            'form' => $form->createView(),
         ]);
     }
 
     #[Route(path: '/logout', name: 'dev_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new \LogicException('Problem with logging out');
     }
 }
