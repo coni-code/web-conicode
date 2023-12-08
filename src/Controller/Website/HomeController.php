5b7719000757fe44505a7755f6e49c123886efc6
bartek3xe <bartek3xe@gmail.com>
2023-12-08 22:22:32 +0100

    Prepare templates and controller

--- src/Controller/Website/HomeController.php
+++ src/Controller/Website/HomeController.php
@@ -3,18 +3,28 @@
 namespace App\Controller\Website;
 
 use App\Form\ContactType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('', name: 'app_home')]
-    public function index(): Response
+    #[Route('', name: 'app_home', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
-        $form = $this -> createForm(ContactType::class);
+        $form = $this->createForm(ContactType::class);
+        $form->add('submit', SubmitType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //use symfony/mailer to send emails
+        }
+
         return $this->render('website/home.html.twig',[
-            'contact_form' => $form,
+            'form' => $form,
         ]);
     }
 }
