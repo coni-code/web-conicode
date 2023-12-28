5c8e9534f771b154dad198248cc5cf7ed7c264cc
JozefPotaczek <jpotaczeq@gmail.com>
2023-12-28 20:36:11 +0100

    Create migration for adding meeting statuses

---
+++ migrations/Version20231228191326.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20231228191326 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add meeting statuses for meeting table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE meeting ADD status VARCHAR(255) DEFAULT \'pending\' NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE meeting DROP status');
+    }
+}
