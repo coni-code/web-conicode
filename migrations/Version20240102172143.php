ceeeb4b1cbade689e3e2239b6a2f2e0caa174743
bartek3xe <bartek3xe@gmail.com>
2024-01-02 18:22:07 +0100

    Rewrite migration

--- migrations/Version20240102172143.php
+++ migrations/Version20240102172143.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20240102163043 extends AbstractMigration
+final class Version20240102172143 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -16,9 +16,7 @@ final class Version20240102163043 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649E0F404F5');
-        $this->addSql('DROP INDEX UNIQ_8D93D649E0F404F5 ON user');
-        $this->addSql('ALTER TABLE user CHANGE trello_member_id member_id VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD member_id VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6497597D3FE FOREIGN KEY (member_id) REFERENCES trello_member (id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6497597D3FE ON user (member_id)');
     }
@@ -27,8 +25,6 @@ final class Version20240102163043 extends AbstractMigration
     {
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6497597D3FE');
         $this->addSql('DROP INDEX UNIQ_8D93D6497597D3FE ON user');
-        $this->addSql('ALTER TABLE user CHANGE member_id trello_member_id VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649E0F404F5 FOREIGN KEY (trello_member_id) REFERENCES trello_member (id) ON UPDATE NO ACTION ON DELETE CASCADE');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E0F404F5 ON user (trello_member_id)');
+        $this->addSql('ALTER TABLE user DROP member_id');
     }
 }
