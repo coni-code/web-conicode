e410836a4219797dfba9bef953cfa35b094072d1
JozefPotaczek <jpotaczeq@gmail.com>
2024-01-02 17:59:07 +0100

    Correct migration

--- migrations/Version20240102163043.php
+++ migrations/Version20240102163043.php
@@ -7,19 +7,15 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20240102163043 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add relation between User and Member';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649E0F404F5');
         $this->addSql('DROP INDEX UNIQ_8D93D649E0F404F5 ON user');
         $this->addSql('ALTER TABLE user CHANGE trello_member_id member_id VARCHAR(255) DEFAULT NULL');
@@ -29,7 +25,6 @@ final class Version20240102163043 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6497597D3FE');
         $this->addSql('DROP INDEX UNIQ_8D93D6497597D3FE ON user');
         $this->addSql('ALTER TABLE user CHANGE member_id trello_member_id VARCHAR(255) DEFAULT NULL');
