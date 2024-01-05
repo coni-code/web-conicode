a4a222f9dad6b593a59fba0b07deb60d88b8760b
bartek3xe <bartek3xe@gmail.com>
2024-01-05 23:12:39 +0100

    Handle startDate and endDate parameters in filter

--- src/Service/ApiFilter/MeetingFilter.php
+++ src/Service/ApiFilter/MeetingFilter.php
@@ -5,12 +5,16 @@ namespace App\Service\ApiFilter;
 use ApiPlatform\Doctrine\Orm\Filter\AbstractFilter;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Metadata\Operation;
+use DateTime;
 use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Validator\Constraints\Date;
 
 class MeetingFilter extends AbstractFilter
 {
     private const USER_PROPERTY = 'user';
     private const USERS_PROPERTY = 'users';
+    private const START_DATE_PROPERTY = 'startDate';
+    private const END_DATE_PROPERTY = 'endDate';
 
     protected function filterProperty(
         string $property,
@@ -41,6 +45,18 @@ class MeetingFilter extends AbstractFilter
                 ->andWhere(sprintf('%s IN (:users)', $usersAlias))
                 ->setParameter('users', $userIds);
         }
+
+        if (self::START_DATE_PROPERTY === $property) {
+            $queryBuilder
+                ->andWhere(sprintf('%s.startDate >= :startDate', $alias))
+                ->setParameter('startDate', $value);
+        }
+
+        if (self::END_DATE_PROPERTY === $property) {
+            $queryBuilder
+                ->andWhere(sprintf('%s.endDate <= :endDate', $alias))
+                ->setParameter('endDate', $value);
+        }
     }
 
     public function getDescription(string $resourceClass): array
@@ -58,6 +74,18 @@ class MeetingFilter extends AbstractFilter
                 'required' => false,
                 'description' => 'Filter meetings by a list of user IDs',
             ],
+            self::START_DATE_PROPERTY => [
+                'property' => 'startDate',
+                'type' => 'datetime',
+                'required' => false,
+                'description' => 'Filter meetings after or on this date',
+            ],
+            self::END_DATE_PROPERTY => [
+                'property' => 'endDate',
+                'type' => 'datetime',
+                'required' => false,
+                'description' => 'Filter meetings after or on this date',
+            ]
         ];
     }
 }
