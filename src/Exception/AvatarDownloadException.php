1aa131270d0c17111e5186d3fa1be46c85fa80ce
bartek3xe <bartek3xe@gmail.com>
2024-01-09 20:51:26 +0100

    Code quality

--- src/Exception/AvatarDownloadException.php
+++ src/Exception/AvatarDownloadException.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Exception;
 
 use Exception;
+use GuzzleHttp\Exception\GuzzleException;
 
 class AvatarDownloadException extends Exception
 {
-    public function __construct($message = "", $code = 0, Exception $previous = null)
+    public function __construct($message = "", $code = 0, GuzzleException $previous = null)
     {
         parent::__construct("Avatar Download Error: " . $message, $code, $previous);
     }
