b789434e5581cdb1fe2493d60ed1bfae132e3ab7
Michał <hardcorpl5@gmail.com>
2024-01-12 19:46:05 +0100

    create drop down

--- assets/js/website/navbar.js
+++ assets/js/website/navbar.js
@@ -21,4 +21,18 @@ document.addEventListener('DOMContentLoaded', () => {
             cardElements[index].classList.remove('hovered');
         });
     });
+    handleDropdown();
 });
+
+function handleDropdown() {
+    const navlangElement = document.querySelector('.nav-lang');
+    const dropdownElement = document.querySelector('.home-dropdown-menu');
+
+    if (!dropdownElement) {
+        throw new Error('46b745a7-c816-49a2-aaf3-50266c8cc366');
+    }
+
+    navlangElement.addEventListener('click', () => {
+        dropdownElement.classList.toggle('d-block');
+    });
+}
