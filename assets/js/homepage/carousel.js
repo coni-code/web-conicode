3d2555cd7d77f12987c942bca1b3ec2bee7f9348
KiviGibson <poplawski143@gmail.com>
2024-01-18 16:02:35 +0000

    fix code

--- assets/js/homepage/carousel.js
+++ assets/js/homepage/carousel.js
@@ -1,9 +1,11 @@
 import '@glidejs/glide/dist/glide.min.js';
 
-new Glide('#portfolio', {
+const glide = new Glide('#portfolio', {
     type: 'carousel',
     startAt: 0,
     perView: 1,
     peek: 200,
     focusAt: 'center',
-}).mount();
+});
+
+glide.mount();
