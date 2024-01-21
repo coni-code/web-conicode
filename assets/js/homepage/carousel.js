48812220c2c483fef905e2b67dede155d14fb6cb
KiviGibson <poplawski143@gmail.com>
2024-01-21 00:01:10 +0000

    corect code in carousel.js

--- assets/js/homepage/carousel.js
+++ assets/js/homepage/carousel.js
@@ -1,5 +1,6 @@
 import '@glidejs/glide/dist/glide.min.js';
-const width = screen.width;
+
+const {width} = screen.width;
 const portfolio = new Glide('#portfolio', {
     type: 'carousel',
     startAt: 0,
@@ -12,7 +13,7 @@ portfolio.mount();
 const team = new Glide('#team', {
     type: 'carousel',
     startAt: 1,
-    perView: width <= 900? width <=600? 1 : 2 : 3,
+    perView: width <= 900 ? width <= 600 ? 1 : 2 : 3,
 });
 
 team.mount();
