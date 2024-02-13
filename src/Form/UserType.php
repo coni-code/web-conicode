1742021c26126ad94f61c824bce2b9cf6ce01ea9
JozefPotaczek <jpotaczeq@gmail.com>
2024-02-13 17:46:02 +0100

    Refactor form and handling form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,9 +5,19 @@ declare(strict_types=1);
 namespace App\Form;
 
 use App\Entity\User;
+use App\Enum\PositionEnum;
+use App\Enum\RoleEnum;
+use App\Repository\UserRepository;
+use phpDocumentor\Reflection\PseudoTypes\List_;
+use phpDocumentor\Reflection\Types\Collection;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
@@ -21,27 +31,43 @@ class UserType extends AbstractType
         $builder
             ->add('email', TextType::class)
             ->add('password', PasswordType::class, [
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Password cannot be empty.',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Password should be at least 6 characters long.',
-                    ]),
-                    new Regex([
-                        'pattern' => '/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[-_!@#$%^&*()+]).*$/',
-                        'message' => 'Password must contain at least one digit, one uppercase letter, one lowercase letter, and one special character.',
-                    ]),
-                ],
+                'mapped' => false,
+                'required' => false,
+//                'constraints' => [
+//                    new Length([
+//                        'min' => 6,
+//                        'minMessage' => 'Password should be at least 6 characters long.',
+//                    ]),
+//                    new Regex([
+//                        'pattern' => '/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[-_!@#$%^&*()+]).*$/',
+//                        'message' => 'Password must contain at least one digit, one uppercase letter, one lowercase letter, and one special character.',
+//                    ]),
+//                ],
             ])
+            ->add('name', TextType::class)
+            ->add('surname', TextType::class)
+            ->add('githubLink', UrlType::class, ['required' => false,])
+            ->add('gitlabLink', UrlType::class, ['required' => false,])
+            ->add('linkedinLink', UrlType::class, ['required' => false,])
+            ->add('websiteLink', UrlType::class, ['required' => false,])
+            ->add('youtubeLink', UrlType::class, ['required' => false,])
         ;
+
+        if ($options['isAdmin']) {
+            $builder
+                ->add('positions', ChoiceType::class, [
+                    'choices' => PositionEnum::getChoices(),
+                    'multiple' => true
+                ])
+            ;
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'isAdmin' => false
         ]);
     }
 }
