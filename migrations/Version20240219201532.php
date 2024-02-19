7e778671aa801f610fbfe20b1b89453e37b999f8
KiviGibson <77674694+KiviGibson@users.noreply.github.com>
2024-02-19 21:19:23 +0100

    Update Version20240219201532.php

--- migrations/Version20240219201532.php
+++ migrations/Version20240219201532.php
@@ -7,25 +7,20 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20240219201532 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'allow null as value in position column in user';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user ADD positions JSON DEFAULT NULL, ADD github_link VARCHAR(255) DEFAULT NULL, ADD gitlab_link VARCHAR(255) DEFAULT NULL, ADD linkedin_link VARCHAR(255) DEFAULT NULL, ADD website_link VARCHAR(255) DEFAULT NULL, ADD youtube_link VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user DROP positions, DROP github_link, DROP gitlab_link, DROP linkedin_link, DROP website_link, DROP youtube_link');
     }
 }
