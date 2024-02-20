07b8a39158d13651966cc1a95c9572155c4f8e91
KiviGibson <poplawski143@gmail.com>
2024-02-20 19:43:56 +0000

    correct css file

--- src/Controller/Website/HomeController.php
+++ src/Controller/Website/HomeController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class HomeController extends AbstractController
 {
     public function __construct(
