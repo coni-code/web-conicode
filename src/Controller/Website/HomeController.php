3f89317451e43f3ade20c7bf54b2284f3a4811f5
JozefPotaczek <jpotaczeq@gmail.com>
2024-02-20 19:38:54 +0100

    Create method in UserService

--- src/Controller/Website/HomeController.php
+++ src/Controller/Website/HomeController.php
@@ -5,14 +5,21 @@ declare(strict_types=1);
 namespace App\Controller\Website;
 
 use App\Form\ContactType;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private readonly UserService $userService,
+    ) {
+    }
+
     #[Route('', name: 'app_home', methods: ['GET', 'POST'])]
     public function index(Request $request): Response
     {
@@ -27,6 +34,7 @@ class HomeController extends AbstractController
 
         return $this->render('website/home.html.twig', [
             'form' => $form,
+            'users' => $this->userService->getAll(),
         ]);
     }
 }
