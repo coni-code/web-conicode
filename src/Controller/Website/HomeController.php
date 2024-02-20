6d62e4d235a273208e5aaba6077725d980934ac6
JozefPotaczek <129151963+JozefPotaczek@users.noreply.github.com>
2024-02-20 21:38:17 +0100

    Revert "Create method in UserService"

--- src/Controller/Website/HomeController.php
+++ src/Controller/Website/HomeController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Controller\Website;
 
 use App\Form\ContactType;
-use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,11 +13,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    public function __construct(
-        private readonly UserService $userService,
-    ) {
-    }
-
     #[Route('', name: 'app_home', methods: ['GET', 'POST'])]
     public function index(Request $request): Response
     {
@@ -33,7 +27,6 @@ class HomeController extends AbstractController
 
         return $this->render('website/home.html.twig', [
             'form' => $form,
-            'users' => $this->userService->getAll(),
         ]);
     }
 }
