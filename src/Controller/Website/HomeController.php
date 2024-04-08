93bd15fbbfe6829e380042b00fd5c2539651c1c8
bartek3xe <bartek3xe@gmail.com>
2024-04-08 18:36:41 +0200

    Setup mailer

--- src/Controller/Website/HomeController.php
+++ src/Controller/Website/HomeController.php
@@ -6,21 +6,24 @@ namespace App\Controller\Website;
 
 use App\Form\ContactType;
 use App\Repository\UserRepository;
+use App\Service\MailerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     public function __construct(
         private readonly UserRepository $userRepository,
+        private readonly MailerService $mailerService,
     ) {
     }
 
     #[Route('', name: 'app_home', methods: ['GET', 'POST'])]
-    public function index(Request $request): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
         $form->add('submit', SubmitType::class);
@@ -28,7 +31,10 @@ class HomeController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // use symfony/mailer to send emails
+            $this->mailerService->sendContactUsData($form->getData(), $mailer);
+
+            $this->addFlash('success', 'Siemano');
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('website/home.html.twig', [
---
+++ src/Service/MailerService.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class MailerService
+{
+    public function __construct(private readonly string $contactEmail)
+    {
+    }
+
+    public function sendContactUsData(array $data, MailerInterface $mailer): void
+    {
+        $email = (new Email())
+            ->from($data['email'])
+            ->to($this->contactEmail)
+            ->subject('Wiadomość z formularza kontaktowego')
+            ->text('Nadawca: '.$data['email']."\n\nWiadomość:\n".$data['body']);
+
+        $mailer->send($email);
+    }
+}
