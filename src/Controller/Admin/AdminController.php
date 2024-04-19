2f810526a278b1570a891e76ad941a4c2b08af8a
JozefPotaczek <jpotaczeq@gmail.com>
2024-04-19 16:35:05 +0200

    Pass latest sprint into twig template

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -4,15 +4,26 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use App\Repository\SprintRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
+    public function __construct(
+        private readonly SprintRepository $sprintRepository,
+    ){
+    }
+
     #[Route('/admin', name: 'dev_admin')]
     public function index(): Response
     {
-        return $this->render('admin/dashboard/index.html.twig');
+        $latestSprint = $this->sprintRepository->findLatestSprint();
+
+        return $this->render('admin/dashboard/index.html.twig',
+        [
+            'latest_sprint' => $latestSprint,
+        ]);
     }
 }
